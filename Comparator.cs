176a4bfdbcca2b6cb9f3dea6dbdbc638fae12f5d
tedecti <wolftheguy08@gmail.com>
2024-04-03 21:55:34 +0500

    Comparator (for sorting IPs)

---
+++ Comparator.cs
@@ -0,0 +1,49 @@
+﻿using static System.Int32;
+
+namespace TestEffectiveMobile;
+
+public static class Comparator
+{
+    public static int IpComparator(string a, string b)
+    {
+        var octetsA = a.Trim().Split('.');
+        var octetsB = b.Trim().Split('.');
+        
+        if (octetsA.SequenceEqual(octetsB))
+        {
+            return 0;
+        }
+        else if (Parse(octetsA[0]) > Parse(octetsB[0]))
+        {
+            return 1;
+        }
+        else if (Parse(octetsA[0]) < Parse(octetsB[0]))
+        {
+            return -1;
+        }
+        else if (Parse(octetsA[1]) > Parse(octetsB[1]))
+        {
+            return 1;
+        }
+        else if (Parse(octetsA[1]) < Parse(octetsB[1]))
+        {
+            return -1;
+        }
+        else if (Parse(octetsA[2]) > Parse(octetsB[2]))
+        {
+            return 1;
+        }
+        else if (Parse(octetsA[2]) < Parse(octetsB[2]))
+        {
+            return -1;
+        }
+        else if (Parse(octetsA[3]) > Parse(octetsB[3]))
+        {
+            return 1;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+}
