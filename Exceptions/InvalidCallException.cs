290c6c931f4514805936457ab04b3d8cca7ae801
tedecti <wolftheguy08@gmail.com>
2024-04-03 22:42:24 +0500

    Custom exception

---
+++ Exceptions/InvalidCallException.cs
@@ -0,0 +1,9 @@
+﻿namespace TestEffectiveMobile.Exceptions;
+
+public class InvalidCallException : Exception
+{
+    public InvalidCallException() : base("Invalid call of custom operator")
+    {
+        throw new InvalidOperationException();
+    }
+}
