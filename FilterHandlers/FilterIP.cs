2737ea6b692ba70aa254bff3c8ba3eefdaff4d83
tedecti <wolftheguy08@gmail.com>
2024-04-05 21:12:17 +0500

    final filter (maybe refactor soon)

--- FilterHandlers/FilterIP.cs
+++ FilterHandlers/FilterIP.cs
@@ -1,51 +1,56 @@
 ﻿using System.Globalization;
 using System.Net;
+using TestEffectiveMobile.FileHandlers;
 
 namespace TestEffectiveMobile.FilterHandlers;
 
 public static class FilterIp
 {
-    public static List<string> FilterIpAddresses(List<string> ipAddresses, string[] requestTimes, string addressStart = null, int? addressMask = null, string timeStart = null, string timeEnd = null)
-    {
-        if (ipAddresses.Count != requestTimes.Length)
+    public static List<string> FilterIpAddresses(List<string> ipAddressesWithMasks, string[] requestTimes,
+            string addressStart = null, int? addressMask = null, string timeStart = null, string timeEnd = null)
         {
-            throw new ArgumentException("The count of IP addresses must match the count of request times.");
-        }
+            if (ipAddressesWithMasks.Count != requestTimes.Length)
+            {
+                throw new ArgumentException("The count of IP addresses must match the count of request times.");
+            }
 
-        var filtered = ipAddresses.Zip(requestTimes, (ip, time) => new { Ip = ip, Time = time });
+            var filtered = ipAddressesWithMasks.Zip(requestTimes, (ipWithMask, time) => new { IpWithMask = ipWithMask, Time = time });
 
-        if (!string.IsNullOrEmpty(addressStart))
-        {
-            var lowerBound = AddressToLong(IPAddress.Parse(addressStart));
-            filtered = filtered.Where(x => AddressToLong(IPAddress.Parse(x.Ip)) >= lowerBound);
+            if (!string.IsNullOrEmpty(addressStart))
+            {
+                var lowerBound = AddressToLong(IPAddress.Parse(addressStart));
+                filtered = filtered.Where(x => AddressToLong(IPAddress.Parse(x.IpWithMask.Split('/')[0])) >= lowerBound);
+            }
 
             if (addressMask.HasValue)
             {
-                var upperBound = lowerBound + addressMask.Value;
-                filtered = filtered.Where(x => AddressToLong(IPAddress.Parse(x.Ip)) <= upperBound);
+                filtered = filtered.Where(x => 
+                {
+                    var mask = int.Parse(x.IpWithMask.Split('/')[1]);
+                    return mask >= addressMask.Value;
+                });
             }
-        }
 
-        if (string.IsNullOrEmpty(timeStart) || string.IsNullOrEmpty(timeEnd))
-            return filtered.Select(x => x.Ip).ToList();
-        {
-            var startTime = DateTime.ParseExact(timeStart, "dd.MM.yyyy", CultureInfo.InvariantCulture);
-            var endTime = DateTime.ParseExact(timeEnd, "dd.MM.yyyy", CultureInfo.InvariantCulture);
-            
-            filtered = filtered.Where(x => 
+            if (!string.IsNullOrEmpty(timeStart) && !string.IsNullOrEmpty(timeEnd))
             {
-                var timePart = x.Time.Split(' ')[0];
-                var currentTime = DateTime.ParseExact(timePart, "yyyy-MM-dd", CultureInfo.InvariantCulture);
-                return currentTime >= startTime && currentTime <= endTime;
-            });
+                var startTime = DateTime.ParseExact(timeStart, "dd.MM.yyyy", CultureInfo.InvariantCulture);
+                var endTime = DateTime.ParseExact(timeEnd, "dd.MM.yyyy", CultureInfo.InvariantCulture);
+
+                filtered = filtered.Where(x =>
+                {
+                    var timePart = x.Time.Split(' ')[0];
+                    var currentTime = DateTime.ParseExact(timePart, "yyyy-MM-dd", CultureInfo.InvariantCulture);
+                    return currentTime >= startTime && currentTime <= endTime;
+                });
+            }
+
+            // Возвращаем IP-адреса с масками подсети после всех фильтраций
+            return filtered.Select(x => x.IpWithMask).ToList();
         }
 
-        return filtered.Select(x => x.Ip).ToList();
-    }
-
-    private static long AddressToLong(IPAddress ip)
-    {
-        var bytes = ip.GetAddressBytes();
-        return bytes.Aggregate<byte, long>(0, (current, t) => (current << 8) + t);
-    }
+        private static long AddressToLong(IPAddress ip)
+        {
+            var bytes = ip.GetAddressBytes();
+            return bytes.Aggregate<byte, long>(0, (current, t) => (current << 8) + t);
+        }
 }
