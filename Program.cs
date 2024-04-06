f6f343b1c9edd912eb5ad27b512e51e9413eada0
tedecti <wolftheguy08@gmail.com>
2024-04-06 14:12:48 +0500

    Final result

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using TestEffectiveMobile.Exceptions;
+﻿using System.Text.Json;
+using TestEffectiveMobile.Exceptions;
+using TestEffectiveMobile.FileHandlers;
 
 namespace TestEffectiveMobile;
 
@@ -6,20 +8,40 @@ internal static class Program
 {
     public static void Main(string[] args)
     {
-        var parameters = ParseArguments(args);
-        //TODO: попробовать улучшить колбасу
-        if (!parameters.ContainsKey("file-log") || !parameters.ContainsKey("file-output"))
+        const string configPath = @"public\config.json";
+        if (!File.Exists(configPath)) return;
+        var jsonString = File.ReadAllText(configPath);
+        var config = JsonSerializer.Deserialize<Configuration>(jsonString);
+        if (config != null)
         {
-            Console.WriteLine("Usage: --file-log=<path> --file-output=<path> [--address-start=<IP>] [--address-mask=<mask>]");
-            return;
+            if (string.IsNullOrEmpty(config.FileLog) || string.IsNullOrEmpty(config.FileOutput))
+            {
+                throw new InvalidConfigException();
+            }
+            WriteFile.WriteIp(config.FileLog, config.FileOutput, config.AddressStart, config.AddressMask, config.TimeStart, config.TimeEnd);
         }
-        var fileLog = parameters["file-log"];
-        var fileOutput = parameters["file-output"];
-        var addressStart = parameters.ContainsKey("address-start") ? parameters["address-start"] : null;
-        var addressMask = parameters.ContainsKey("address-mask") ? parameters["address-mask"] : null;
+        else
+        {
+            var parameters = ParseArguments(args);
+            if (!parameters.ContainsKey("file-log") || !parameters.ContainsKey("file-output"))
+            {
+                Console.WriteLine(
+                    "Usage: --file-log=<path> --file-output=<path> [--address-start=<IP>] [--address-mask=<mask>]");
+                return;
+            }
+
+            var fileLog = parameters["file-log"];
+            var fileOutput = parameters["file-output"];
+            var addressStart = parameters.ContainsKey("address-start") ? parameters["address-start"] : null;
+            var addressMask = int.Parse(parameters.ContainsKey("address-mask") ? parameters["address-mask"] : null);
+            var timeStart = parameters["time-start"];
+            var timeEnd = parameters["time-end"];
+
+            WriteFile.WriteIp(fileLog, fileOutput, addressStart, addressMask, timeStart, timeEnd);
+        }
     }
 
-    static Dictionary<string, string> ParseArguments(string[] args)
+    private static Dictionary<string, string> ParseArguments(string[] args)
     {
         var parameters = new Dictionary<string, string>(StringComparer.Ordinal);
         foreach (var arg in args)
