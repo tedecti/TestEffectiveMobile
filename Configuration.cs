450fb34c946ca6053ba90ff1bdbf0de613df08e1
tedecti <wolftheguy08@gmail.com>
2024-04-06 14:12:30 +0500

    Additional configuration

---
+++ Configuration.cs
@@ -0,0 +1,12 @@
+﻿namespace TestEffectiveMobile;
+
+public class Configuration
+{
+    public string FileLog { get; set; }
+    public string FileOutput { get; set; }
+    public string AddressStart { get; set; }
+    public int AddressMask { get; set; }
+    public string TimeStart { get; set; }
+    public string TimeEnd { get; set; }
+    
+}
